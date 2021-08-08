cd414697208933ce255f8c5196b8dde2d17eae36
edmarsht <edtoulet@gmail..com>
2021-08-08 11:47:13 +0200

    final actionCard

--- src/Todo.js
+++ src/Todo.js
@@ -75,23 +75,7 @@ function Todo({ todo }) {
         </Modal>
    
         
-        <Card className={classes.root} variant="outlined" gutterBottom>
-        <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-            Go 🚀
-        </Typography>
-        <Typography className={classes.pos} primary={todo.todo} variant="h5" component="h2">
-            {todo.todo}
-        </Typography>
-        <div className="todo__edit">
-                <Button className="button__todo" onClick={e => setOpen(true)}>Edit</Button>
-                <HighlightOffIcon className="icon__todo" onClick={event => db.collection('todos').doc(todo.id).delete()}></HighlightOffIcon>
-            {/* with a delete event  */}
-            </div>
-        </CardContent>
-        <CardActions>
-        </CardActions>
-        </Card>
+
 
 
         <Card className={classes.root}>
